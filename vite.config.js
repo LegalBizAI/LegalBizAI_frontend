ab5bf912ae1c4956912b4b6a2f687fb595b04cb0
Nguyen Hoang Nguyen <iamnghoangnguyen@gmail.com>
2024-07-24 00:39:37 +0700

    Set up deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/LegalbizAi_chatbot', // Đảm bảo rằng base path này đúng
+    base: '/LegalBizAI_frontend/', // Đảm bảo rằng base path này đúng
     plugins: [react()],
     resolve: {
         alias: {
