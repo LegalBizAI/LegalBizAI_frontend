c8e52cf2680c1ec473e094e51d0fcf07e8b076ba
Nguyen Hoang Nguyen <iamnghoangnguyen@gmail.com>
2024-07-24 02:32:51 +0700

    fix: 404.html redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-// src/App.jsx
-import { useEffect, useState } from 'react';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import ChatBotV1 from './components/ChatBot-v1';
 import ChatBot from './components/ChatBot';
 import FAQPage from './pages/FAQPage';
 import IssuePage from './pages/IssuePage';
-import Footer from './components/Footer';
-import { Routes, Route } from 'react-router-dom';
+// import Footer from './components/Footer';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-function App() {
-    useEffect(() => {}, []);
-    const [currentPage, setCurrentPage] = useState('Home');
-
+export default function App() {
     return (
         <div className="flex flex-col min-h-screen">
             <NavBar />
@@ -23,11 +18,10 @@ function App() {
                     <Route path="chat-v1" element={<ChatBotV1 />} />
                     <Route path="issue" element={<IssuePage />} />
                     <Route path="faq" element={<FAQPage />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
             </main>
             {/* <Footer /> */}
         </div>
     );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+console.log('VITE_REPO_NAME', import.meta.env.VITE_REPO_NAME);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter
